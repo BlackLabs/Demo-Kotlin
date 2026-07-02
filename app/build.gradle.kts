898f8ada13ee354c428dda284ab96f17c3d9b464
Braian Avalos <bavalos@blacklabs.mx>
2026-07-02 13:53:58 -0600

    update versionCode and versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "mx.pagandocheck.pagandodemokotlin"
         minSdk = 29
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 11
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
